f70fb6d1bfd5e07514c4f0a7e4eb5285d0147995
victor-heliomar <victordev2002@gmail.com>
2022-09-08 09:45:24 -0400

    Added error messages

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use output::output;
 fn main() {
     let mut args: Args = args();
 
-    let first_number: f32 = args.nth(1).unwrap().parse::<f32>().unwrap();
+    let first_number: f32 = args.nth(1).expect("\nFirst number not found\nPlease remember add the numbers with this format `calculator 2 + 2`\n").parse::<f32>().expect("\nWe had problems to read the first number, remember that should use numbers and should have this format: `calculator 2 + 2`\n");
     let operator: char = args.nth(0).unwrap().chars().next().unwrap();
-    let second_number: f32 = args.nth(0).unwrap().parse::<f32>().unwrap();
+    let second_number: f32 = args.nth(0).expect("\nSecond number not found\nPlease remember add the numbers with this format `calculator 2 + 2`\n").parse::<f32>().expect("\nWe had problems to read the second number, remember that should use numbers and should have this format: `calculator 2 + 2\n`");
 
     let result = operate(operator, first_number, second_number);
     
